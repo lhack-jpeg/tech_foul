7c1efc81930a120ac9c08cc4b30f42d3f27679a9
Gotmished <5002@holbertonstudents.com>
2023-02-07 17:06:49 +1100

    Adding what we have currently

--- tech_foul_app/models/match.js
+++ tech_foul_app/models/match.js
@@ -3,19 +3,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TeamSchema = new Schema({
-  team_id: { type: String, required: true },
-  team_name: { type: String, required: true },
-  avg_gold_min: { type: Number, required: true },
-  avg_xp_min: { type: Number, required: true },
-  previous_performance: { type: String, required: true },
-  previous_xp_advantage: { type: Number, required: true },
-  previous_gold_advantage: { type: Number, required: true }
+  avg_match_time: { type: Number, required: true },
+  avg_score: { type: Number, required: true },
+  first_blood_pct: { type: Number, required: true },
+  prev_form: { type: String, required: true }
 });
 
 const MatchSchema = new Schema({
-  match_id: { type: String, required: true },
-  teams: [TeamSchema]
+  match_id: String,
+  team_one_stats: [TeamSchema],
+  team_two_stats: [TeamSchema]
 });
 
-// Export model
 module.exports = mongoose.model('Match', MatchSchema);
