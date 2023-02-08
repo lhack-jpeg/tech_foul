88f2d99675995b0d78b0c5d59bb59794edab8381
lhack-jpeg <luke-hackett@outlook.com>
2023-02-09 10:42:36 +1100

    remove unused code

--- tech_foul_app/services/mysqlDB.js
+++ tech_foul_app/services/mysqlDB.js
@@ -1,23 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-// const conn = mysql.createConnection({
-//     host: process.env.MYSQL_DB_HOST,
-//     port: '3306',
-//     user: process.env.MYSQL_DB_USER,
-//     password: process.env.MYSQL_DB_PASS,
-//     database: process.env.MYSQL_DB,
-// });
-
-// conn.connect(function (err) {
-//     if (err) {
-//         return console.error(`error: ${err.message}`);
-//     }
-//     console.log('Connected to server');
-// });
-
-// module.exports = conn;
-
 const sequelize = new Sequelize(
     process.env.MYSQL_DB,
     process.env.MYSQL_DB_USER,
