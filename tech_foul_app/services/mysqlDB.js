fc7564a90473ace33523bc22b47db7bd29dd8e03
lhack-jpeg <luke-hackett@outlook.com>
2023-02-08 16:01:09 +1100

    add new datasource connection

--- tech_foul_app/services/mysqlDB.js
+++ tech_foul_app/services/mysqlDB.js
@@ -1,19 +1,38 @@
-const mysql = require('mysql2');
 require('dotenv').config();
+const { DataSource } = require('typeorm');
 
-const conn = mysql.createConnection({
+// const conn = mysql.createConnection({
+//     host: process.env.MYSQL_DB_HOST,
+//     port: '3306',
+//     user: process.env.MYSQL_DB_USER,
+//     password: process.env.MYSQL_DB_PASS,
+//     database: process.env.MYSQL_DB,
+// });
+
+// conn.connect(function (err) {
+//     if (err) {
+//         return console.error(`error: ${err.message}`);
+//     }
+//     console.log('Connected to server');
+// });
+
+// module.exports = conn;
+
+const AppDataSource = new DataSource({
+    type: 'mysql',
     host: process.env.MYSQL_DB_HOST,
     port: '3306',
-    user: process.env.MYSQL_DB_USER,
+    username: process.env.MYSQL_DB_USER,
     password: process.env.MYSQL_DB_PASS,
     database: process.env.MYSQL_DB,
 });
 
-conn.connect(function (err) {
-    if (err) {
-        return console.error(`error: ${err.message}`);
-    }
-    console.log('Connected to server');
-});
+AppDataSource.initialize()
+    .then(() => {
+        console.log('Data Source has been initialized!');
+    })
+    .catch((err) => {
+        console.error(`Error during initalisation process: ${err}`);
+    });
 
-module.exports = conn;
+module.exports = AppDataSource;
