a524a92c4e44e391d9639d3401ab31112cf04633
lhack-jpeg <luke-hackett@outlook.com>
2023-02-09 10:42:09 +1100

    use sequelize to retreive all matches from mysql

--- tech_foul_app/controllers/index.js
+++ tech_foul_app/controllers/index.js
@@ -1,52 +1,27 @@
-const AppDataSource = require('../services/mysqlDB');
-const AppError = require('../utilities/expressError');
 const moment = require('moment');
 const Matches = require('../models/match');
-const sqlMatch = require('../models/matches');
+const sqlMatch = require('../models/sqlMatches');
 const Team = require('../models/teams');
+const { log } = require('console');
 
 exports.getAllMatches = async (req, res, next) => {
-    const matches = await AppDataSource.getRepository(sqlMatch)
-        .createQueryBuilder('match')
-        .leftJoinAndSelect(
-            Team.logo_url,
-            'teams',
-            'teams.id = match.team_one_id'
-        )
-        .getMany();
-    console.log(matches);
-    res.send(matches);
-    // conn.query(
-    //     'SELECT\
-    //     matches.id,\
-    //     team_one,\
-    //     team_two,\
-    //     match_format,\
-    //     epoch_time,\
-    //     tournament_name,\
-    //     t1.logo_url as team_one_logo,\
-    //     t2.logo_url as team_two_logo\
-    // FROM\
-    //     matches\
-    // LEFT JOIN\
-    //     `teams` t1 ON (t1.id = matches.team_one_id)\
-    // LEFT JOIN\
-    //     `teams` t2 ON (t2.id = matches.team_two_id)',
-    //     // To send a view you need to use res.render('route/to/view', {data to be sent through})
-    //     function (error, results, fields) {
-    //         if (error) return next(new AppError(error));
-
-    //         // Convert the `epoch_time` to the start time of the match in `hh:mm:ss` format using moment
-    //         results.forEach((result) => {
-    //             result.startTime = moment(result.epoch_time * 1000).format(
-    //                 'HH:mm:ss'
-    //             );
-    //         });
-
-    //         res.render('pages/homepage', { results });
-    //         console.log(results);
-    //     }
-    // )
+    const results = await sqlMatch.findAll({
+        include: [
+            {
+                model: Team,
+                as: 'team_one_info',
+            },
+            {
+                model: Team,
+                as: 'team_two_info',
+            },
+        ],
+    });
+    results.forEach((result) => {
+        result.startTime = moment(result.epoch_time * 1000).format('HH:mm:ss');
+    });
+    log(JSON.stringify(results));
+    res.render('pages/homepage', { results });
 };
 
 // Display detail of a match that has been clicked on
