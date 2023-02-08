bf99ca7bc26c5e34d23abfa415342eb471388b3f
lhack-jpeg <luke-hackett@outlook.com>
2023-02-08 14:25:49 +1100

    move files to lower level

--- mongo_link/tests/test_models/test_mongo.py
+++ mongo_link/tests/test_models/test_mongo.py
@@ -3,10 +3,10 @@ import inspect
 import os, sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from ..sql_model import Base, Match
-import variables as DB
+from sql_model import Base, Match
 import urllib.parse
 import sql_mongo_link
+import variables as DB
 from mongo_db_connect import get_mongoDB
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
@@ -17,6 +17,7 @@ class TestMongo(unittest.TestCase):
     Unit tests for mongo data link between sql and mongoDB
     """
 
+    @classmethod
     def setUpClass(cls):
         """Get functions from file"""
         cls.mongo_funcs = inspect.getmembers(sql_mongo_link, inspect.isfunction)
@@ -26,21 +27,12 @@ class TestMongo(unittest.TestCase):
         Set up test class.
         """
         self.engine = create_engine(
-            f"mysql+mysqldb://{DB.MYSQL_DB_USER}:\
-                {urllib.parse.quote(DB.MYSQL_DB_PASS)}@\
-                {DB.MYSQL_DB_HOST}/{DB.MYSQL_DB}",
+            f"mysql+mysqldb://{DB.MYSQL_DB_USER}:{urllib.parse.quote(DB.MYSQL_DB_PASS)}@{DB.MYSQL_DB_HOST}/{DB.MYSQL_DB}",
             pool_pre_ping=True,
         )
         self.session = Session(bind=self.engine)
-        Base.metadata.create_all(self.engine)
-        self.mongo_connection = get_mongoDB()
-
-    def tearDown(self):
-        """
-        Breaks down the test session
-        """
-        Base.metadata.drop_all(self.engine)
-        self.mongo_connection.close()
+        self.mongo_DB = get_mongoDB()
+        self.mongo_matches = self.mongo_DB["matches"]
 
     def test_docs(self):
         """Test sql_mongo_link_docs."""
@@ -56,15 +48,27 @@ class TestMongo(unittest.TestCase):
         """Test the return result of SQL query."""
         result = self.session.query(Match).first()
         self.assertIsNotNone(result)
-        self.assertIsInstance(type(result.id), type("String"))
-        self.assertEqual(type(int), type(result.team_one_id))
+        self.assertEqual(type(result.id), type("String"))
+        self.assertEqual(int, type(result.team_one_id))
 
     def test_mongo_result(self):
         """Test to ensure that match info from mongoDB."""
         mysql_result = self.session.query(Match).first()
-        mongo_conn = get_mongoDB()
-        mongo_result = mongo_conn.findOne({"match_id": mysql_result.id})
+        mongo_result = self.mongo_matches.find_one(
+            {"match_id": mysql_result.id}
+        )
         self.assertIsNotNone(mongo_result)
+        self.assertEqual(mongo_result.get("match_id"), mysql_result.id)
+
+    def test_mongo_match_keys(self):
+        """test the keys from the mongo match stored."""
+        obj_keys = ["_id", "match_id", "epoch_time", "team_one", "team_two"]
+        mysql_result = self.session.query(Match).first()
+        mongo_result = self.mongo_matches.find_one(
+            {"match_id": mysql_result.id}
+        )
+        for key in mongo_result.keys():
+            self.assertIn(key, obj_keys)
 
 
 if __name__ == "__main__":
