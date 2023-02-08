f114cd034d4fa3c2405b2eadff3e4230ff2a360d
Gotmished <5002@holbertonstudents.com>
2023-02-08 12:21:08 +1100

    Adding statistics to be represented

--- tech_foul_app/models/match.js
+++ tech_foul_app/models/match.js
@@ -3,16 +3,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TeamSchema = new Schema({
-    avg_match_time: { type: Number, required: true },
-    avg_score: { type: Number, required: true },
-    first_blood_pct: { type: Number, required: true },
-    prev_form: { type: String, required: true },
+  name: { type: String, required: true },
+  team_id: { type: Number, required: true },
+  rating: { type: Number, required: true },
+  wins: { type: Number, required: true },
+  losses: { type: Number, required: true },
+  avg_match_time: { type: Number, required: true },
+  avg_score: { type: Number, required: true },
+  first_blood_pct: { type: Number, required: true },
+  prev_form: { type: String, required: true },
+  elo_win_pct: { type: Number, required: true }
 });
 
 const MatchSchema = new Schema({
-    match_id: String,
-    team_one_stats: [TeamSchema],
-    team_two_stats: [TeamSchema],
+  match_id: String,
+  team_one_stats: [TeamSchema],
+  team_two_stats: [TeamSchema]
 });
 
 // Syntax is model name, schema name, collection name
