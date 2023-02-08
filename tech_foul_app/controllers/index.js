92fa4755be238300f776c221ffdb91a9175fea90
RubberizedDuck <dyl_2536@hotmail.com>
2023-02-09 10:48:09 +1100

    Update startTime function to show timeUntilStart

--- tech_foul_app/controllers/index.js
+++ tech_foul_app/controllers/index.js
@@ -3,8 +3,8 @@ const AppError = require('../utilities/expressError');
 const moment = require('moment');
 
 exports.getAllMatches = (req, res, next) => {
-    conn.query(
-        'SELECT\
+	conn.query(
+		'SELECT\
         matches.id,\
         team_one,\
         team_two,\
@@ -19,17 +19,28 @@ exports.getAllMatches = (req, res, next) => {
         `teams` t1 ON (t1.id = matches.team_one_id)\
     LEFT JOIN\
         `teams` t2 ON (t2.id = matches.team_two_id)',
-        // To send a view you need to use res.render('route/to/view', {data to be sent through})
-        function (error, results, fields) {
-            if (error) return next(new AppError(error));
+		// To send a view you need to use res.render('route/to/view', {data to be sent through})
+		function (error, results, fields) {
+			if (error) return next(new AppError(error));
 
-            // Convert the `epoch_time` to the start time of the match in `hh:mm:ss` format using moment
-            results.forEach((result) => {
-                result.startTime = moment(result.epoch_time * 1000).format('HH:mm:ss');
-            });
+			// Convert the `epoch_time` to the start time of the match in `hh:mm:ss` format using moment
+			results.forEach((result) => {
+				const startTime = new Date(result.epoch_time * 1000);
+				const currentTime = new Date();
+				const timeUntilStart = startTime - currentTime;
 
-            res.render('pages/homepage', { results });
-            console.log(results);
-        }
-    );
+				const minutesUntilStart = Math.floor(timeUntilStart / 1000 / 60 / 60);
+				const secondsUntilStart = Math.floor((timeUntilStart / 1000 / 60) % 60);
+
+				if (timeUntilStart <= 0) {
+					result.timeUntilStart = 'Game Finished';
+				} else {
+					result.timeUntilStart = `${minutesUntilStart} hours and ${secondsUntilStart} minutes`;
+				}
+			});
+
+			res.render('pages/homepage', { results });
+			console.log(results);
+		}
+	);
 };
