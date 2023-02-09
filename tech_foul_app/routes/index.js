f3e2b98c524d6e9d3886753c98b46c551fb53d3a
lhack-jpeg <luke-hackett@outlook.com>
2023-02-10 10:37:27 +1100

    add post route

--- tech_foul_app/routes/index.js
+++ tech_foul_app/routes/index.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const controllers = require('../controllers')
-const wrapAsync = require('../utilities/wrapAsync')
-const router = express.Router()
+const express = require('express');
+const controllers = require('../controllers');
+const wrapAsync = require('../utilities/wrapAsync');
+const router = express.Router();
 
-router.route('/').get(wrapAsync(controllers.getAllMatches))
-router.route('/:match_id').get(wrapAsync(controllers.match_detail))
+router.route('/').get(wrapAsync(controllers.getAllMatches));
+router.route('/api').post(wrapAsync(controllers.getTeamData));
+router.route('/:match_id').get(wrapAsync(controllers.match_detail));
 
-module.exports = router
+module.exports = router;
