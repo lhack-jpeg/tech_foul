8da41e9f4683581699c2cb20a9f07b9736c74a8c
lhack-jpeg <luke-hackett@outlook.com>
2023-02-09 14:13:35 +1100

    add wrapasync function

--- tech_foul_app/routes/index.js
+++ tech_foul_app/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const controllers = require('../controllers');
+const wrapAsync = require('../utilities/wrapAsync');
 const router = express.Router();
 
-router.route('/').get(controllers.getAllMatches);
-router.route('/:match_id').get(controllers.match_detail);
+router.route('/').get(wrapAsync(controllers.getAllMatches));
+router.route('/:match_id').get(wrapAsync(controllers.match_detail));
 
 module.exports = router;
