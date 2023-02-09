acffea6e986a132d71386cfd67d368f39cebd97f
Gotmished <5002@holbertonstudents.com>
2023-02-09 16:56:55 +1100

    Updated match model according to new MongoDB entries

--- tech_foul_app/models/match.js
+++ tech_foul_app/models/match.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TeamSchema = new Schema({
+  id: { type: Number, required: true },
   name: { type: String, required: true },
-  team_id: { type: Number, required: true },
   rating: { type: Number, required: true },
   wins: { type: Number, required: true },
   losses: { type: Number, required: true },
