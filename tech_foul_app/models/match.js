aa220328f035ca0e5dbe5f70ce31a28bbc4355b5
lhack-jpeg <luke-hackett@outlook.com>
2023-02-14 12:59:39 +1100

    change schema name

--- tech_foul_app/models/match.js
+++ tech_foul_app/models/match.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const TeamSchema = new Schema({
-  id: { type: Number, required: true },
+  team_id: { type: Number, required: true },
   name: { type: String, required: true },
   rating: { type: Number, required: true },
   wins: { type: Number, required: true },
@@ -12,14 +12,14 @@ const TeamSchema = new Schema({
   avg_score: { type: Number, required: true },
   first_blood_pct: { type: Number, required: true },
   prev_form: { type: String, required: true },
-  elo_win_pct: { type: Number, required: true }
-})
+  elo_win_pct: { type: Number, required: true },
+});
 
 const MatchSchema = new Schema({
   match_id: { type: String, required: true },
   team_one: [TeamSchema],
-  team_two: [TeamSchema]
-})
+  team_two: [TeamSchema],
+});
 
 // Syntax is model name, schema name, collection name
-module.exports = mongoose.model('Matches', MatchSchema, 'matches')
+module.exports = mongoose.model('Matches', MatchSchema, 'matches');
