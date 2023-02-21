e745313845d3f2bedb132a9bac112ff514506c11
RubberizedDuck <dyl_2536@hotmail.com>
2023-02-21 12:08:46 +1100

    Add new vars for single match page and comments

--- tech_foul_app/public/javascript/toggleLightDark.js
+++ tech_foul_app/public/javascript/toggleLightDark.js
@@ -1,7 +1,13 @@
 window.onload = () => {
+  // For Homepage
   const toggleMode = document.getElementById('darkmode-toggle');
   const body_element = document.getElementById('body-light');
   const main_element = document.getElementById('main-light');
+  // For Single Match Page
+  const matchToggleMode = document.getElementById('match-darkmode-toggle');
+  const match_body_element = document.getElementById('match-body-light');
+  const match_main_element = document.getElementById('match-main-light');
+  // Selects all in both html files
   const textMainSwitch = document.querySelectorAll('.main-text');
   const headerMainSwitch = document.querySelectorAll('.main-header');
   const textLightSwitch = document.querySelectorAll('.light-mode-text');
@@ -14,31 +20,35 @@ window.onload = () => {
       main_element.classList.toggle('light-mode-main');
       iterateChangeElement(textMainSwitch, 'light-mode-text', 'main-text');
       iterateChangeElement(textLightSwitch, 'light-mode-text', 'main-text');
-      iterateChangeElement(
-        headerMainSwitch,
-        'light-mode-header',
-        'main-header'
-      );
-      iterateChangeElement(
-        headerLightSwitch,
-        'light-mode-header',
-        'main-header'
-      );
+      iterateChangeElement(headerMainSwitch, 'light-mode-header', 'main-header');
+      iterateChangeElement(headerLightSwitch, 'light-mode-header', 'main-header');
     } else {
       body_element.classList.toggle('light-mode-body');
       main_element.classList.toggle('light-mode-main');
       iterateChangeElement(textMainSwitch, 'main-text', 'light-mode-text');
       iterateChangeElement(textLightSwitch, 'main-text', 'light-mode-text');
-      iterateChangeElement(
-        headerMainSwitch,
-        'main-header',
-        'light-mode-header'
-      );
-      iterateChangeElement(
-        headerLightSwitch,
-        'main-header',
-        'light-mode-header'
-      );
+      iterateChangeElement(headerMainSwitch, 'main-header', 'light-mode-header');
+      iterateChangeElement(headerLightSwitch, 'main-header', 'light-mode-header');
+      console.log('Unchecked');
+    }
+  });
+  matchToggleMode.addEventListener('click', function () {
+    console.log({ matchToggleMode });
+    if (matchToggleMode.checked) {
+      // When toggle is checked, dark mode is enabled
+      body_element.classList.toggle('light-mode-body');
+      main_element.classList.toggle('light-mode-main');
+      iterateChangeElement(textMainSwitch, 'light-mode-text', 'main-text');
+      iterateChangeElement(textLightSwitch, 'light-mode-text', 'main-text');
+      iterateChangeElement(headerMainSwitch, 'light-mode-header', 'main-header');
+      iterateChangeElement(headerLightSwitch, 'light-mode-header', 'main-header');
+    } else {
+      body_element.classList.toggle('light-mode-body');
+      main_element.classList.toggle('light-mode-main');
+      iterateChangeElement(textMainSwitch, 'main-text', 'light-mode-text');
+      iterateChangeElement(textLightSwitch, 'main-text', 'light-mode-text');
+      iterateChangeElement(headerMainSwitch, 'main-header', 'light-mode-header');
+      iterateChangeElement(headerLightSwitch, 'main-header', 'light-mode-header');
       console.log('Unchecked');
     }
   });
@@ -51,11 +61,7 @@ window.onload = () => {
  * @classNameOne - Class name to check if exist on object and pass through to call back
  * @classNameTwo - Class name to add to html object
  */
-const iterateChangeElement = (
-  listOfHtmlElement,
-  classNameOne,
-  classNameTwo
-) => {
+const iterateChangeElement = (listOfHtmlElement, classNameOne, classNameTwo) => {
   listOfHtmlElement.forEach((htmlElement) => {
     addRemoveClassName(htmlElement, classNameOne, classNameTwo);
   });
