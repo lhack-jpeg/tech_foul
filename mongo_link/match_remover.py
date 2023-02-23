e60252446685c74e2e08457bef1830201ed79f5a
lhack-jpeg <luke-hackett@outlook.com>
2023-02-23 17:42:58 +1100

    add process to remove matches from mysql and mongo

--- mongo_link/match_remover.py
+++ mongo_link/match_remover.py
@@ -3,16 +3,22 @@ This module removes matches that are 5 hours past their start time
 """
 
 from sql_mongo_link import get_sql_matches
+from sqlalchemy.orm import sessionmaker
+from sql_model import mysql_connect
 from datetime import datetime, timedelta
 from mongo_db_connect import get_mongoDB
 
 mongoDB = get_mongoDB()
+matches_collection = mongoDB["matches"]
 # Get current datetime as timestamp
 current_datetime = datetime.now()
 time_difference = timedelta(hours=5)
 
 
 if __name__ == "__main__":
+    engine = mysql_connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
     matches = list(get_sql_matches())
     for match in matches:
         # Convert unix time stamp to datetime
@@ -21,7 +27,6 @@ if __name__ == "__main__":
         match_time_5_hours = match_time + time_difference
         # Check if greater than 5 hours difference
         if current_datetime > match_time_5_hours:
-            # Will need to change to remove from database
-            # Remove from mongodb with mongo delete
-            # delete from sql database using sql begin
-            print("True")
+            session.delete(match)
+            session.commit()
+            matches_collection.delete_one({"match_id": match.id})
