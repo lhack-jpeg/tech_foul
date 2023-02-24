8eb6181c5013912a37998bb4be5c46227e12d4d7
Luke Hackett <luke-hackett@outlook.com>
2023-02-24 12:50:53 +1100

    create two graphs instead of 1

--- tech_foul_app/public/javascript/eloChart.js
+++ tech_foul_app/public/javascript/eloChart.js
@@ -1,5 +1,8 @@
-const ctx = document.getElementById('eloChart');
+const ctxOne = document.getElementById('eloChartTeamOne');
+const ctxTwo = document.getElementById('eloChartTeamTwo');
 const teamOneText = document.getElementById('teamOneID').innerText;
+const teamOneName = document.getElementById('team-one-name').innerText;
+const teamTwoName = document.getElementById('team-two-name').innerText;
 const teamTwoText = document.getElementById('teamTwoID').innerText;
 const splintString = (string) => {
   const indexOfChar = string.indexOf(':');
@@ -10,67 +13,89 @@ const splintString = (string) => {
 const teamOneId = splintString(teamOneText);
 const teamTwoId = splintString(teamTwoText);
 // console.log('team info', teamOneId, teamTwoId);
-async function postData (url = '', teamOneID, teamTwoID) {
+async function postData(url = '', teamID) {
   const response = await fetch(url, {
     method: 'POST',
     mode: 'cors',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      teamOne: teamOneID,
-      teamTwo: teamTwoID
-    })
+      team: teamID,
+      teamTwo: teamTwoID,
+    }),
   });
   const teamData = response.json();
   return teamData;
 }
 
-const teamData = postData(
-  'http://localhost:4000/api',
-  teamOneId,
-  teamTwoId
-).then((value) => {
-  new Chart(ctx, {
-    type: 'line',
-    data: {
-      datasets: [
-        {
-          label: 'team_one_rating',
-          xAxisID: 'teamOne',
-          data: value.teamOne.map((row) => ({
-            x: row.inserted_at,
-            y: row.rating
-          })),
-          tension: 0.5
-        },
-        {
-          label: 'team_two_rating',
-          xAxisID: 'teamTwo',
-          data: value.teamTwo.map((row) => ({
-            x: row.inserted_at,
-            y: row.rating
-          })),
-          tension: 0.5
-        }
-      ]
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      scales: {
-        xAxes:
+const teamOneData = postData('http://localhost:4000/api', teamOneId).then(
+  (value) => {
+    new Chart(ctxOne, {
+      type: 'line',
+      data: {
+        datasets: [
           {
+            label: teamOneName,
+            data: value.team.map((row) => ({
+              x: row.inserted_at,
+              y: row.rating,
+            })),
+            tension: 0.5,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
             scaleLabel: {
               display: true,
-              labelString: 'Date'
+              labelString: 'Date',
             },
+          },
+          y: {
+            beginAtZero: false,
+          },
+        },
+      },
+    });
+  }
+);
+
+const teamTwoData = postData('http://localhost:4000/api', teamTwoId).then(
+  (value) => {
+    new Chart(ctxTwo, {
+      type: 'line',
+      data: {
+        datasets: [
+          {
+            label: teamTwoName,
+            data: value.team.map((row) => ({
+              x: row.inserted_at,
+              y: row.rating,
+            })),
+            borderColor: '#fc4c7c',
+            tension: 0.5,
+            fill: {
+              display: true,
+              target: 'origin',
+              below: '#fc4c7c',
+            },
+          },
+        ],
+      },
+      options: {
+        scales: {
+          x: {
             ticks: {
-              source: 'auto',
-              display: false
-            }
-          }
-      }
-    }
-  });
-});
+              source: 'labels',
+            },
+          },
+          y: {
+            beginAtZero: false,
+          },
+        },
+      },
+    });
+  }
+);
