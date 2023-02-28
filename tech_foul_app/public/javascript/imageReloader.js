e68a9add898e4ceea09ee5806b1eeae9f8d0f20c
lhack-jpeg <luke-hackett@outlook.com>
2023-02-28 14:15:46 +1100

    change var name

--- tech_foul_app/public/javascript/imageReloader.js
+++ tech_foul_app/public/javascript/imageReloader.js
@@ -1,6 +1,6 @@
 const images = document.getElementsByTagName('img');
-for (const i of images) {
-  i.addEventListener('error', function (event) {
+for (const img of images) {
+  img.addEventListener('error', function (event) {
     event.target.src = '/images/Dota-2-Logo.png';
     event.onerror = null;
   });
