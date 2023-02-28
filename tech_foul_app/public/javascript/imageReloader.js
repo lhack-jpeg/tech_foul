2d02f0000a85925e0a92449836d726ee2f3a8be2
Luke Hackett <luke-hackett@outlook.com>
2023-03-01 10:17:24 +1100

    add debug for print image

--- tech_foul_app/public/javascript/imageReloader.js
+++ tech_foul_app/public/javascript/imageReloader.js
@@ -1,4 +1,5 @@
 const images = document.getElementsByTagName('img');
+console.log(images);
 for (const img of images) {
   img.addEventListener('error', function (event) {
     event.target.src = '/images/Dota-2-Logo.png';
