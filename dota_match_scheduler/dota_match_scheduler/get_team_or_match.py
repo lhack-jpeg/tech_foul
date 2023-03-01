325f50be730112455da91a0f99de0eba57e8b3c3
Luke Hackett <luke-hackett@outlook.com>
2023-03-02 10:16:31 +1100

    functions to query Team and Match Table in MySQL

---
+++ dota_match_scheduler/dota_match_scheduler/get_team_or_match.py
@@ -0,0 +1,28 @@
+'''
+File contains the functions to query the database from Team or Matches
+'''
+
+from dota_match_scheduler.models import db_connect, Team, Match
+from sqlalchemy.orm import sessionmaker
+
+engine = db_connect()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def get_team_id(team_name):
+    '''
+    Takes a team_name and queries the database, returns either
+    the first row
+    '''
+    team_id = session.query(Team.id).filter(Team.name == team_name).first()
+    return team_id[0]
+
+
+def get_match_id(match_id):
+    '''
+    Take a match_id and queries the database. Returns one or None.
+    '''
+    match_row = session.query(Match.id).filter(
+        Match.id == match_id).one_or_none()
+    return match_row
