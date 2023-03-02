be93ef2117c15807deb91c8902270137d015ab9b
Luke Hackett <luke-hackett@outlook.com>
2023-03-02 11:05:23 +1100

    import module to connect to mongo db

---
+++ dota_match_scheduler/dota_match_scheduler/mongo_db_connect.py
@@ -0,0 +1,27 @@
+"""
+This module returns a connection instance to a mongoDB server.
+"""
+from pymongo import MongoClient
+import certifi
+from os import getenv, path
+
+if path.exists("./variables.py"):
+    import variables
+
+
+def get_mongoDB():
+    """Creates a connection to MongoDB database"""
+    if getenv("MONGO_DB_CONNECTION_STRING"):
+        CONNECTION_STRING = getenv("MONGO_DB_CONNECTION_STRING")
+    else:
+        CONNECTION_STRING = variables.MONGO_DB_CONNECTION_STRING
+
+    client = MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where())
+
+    return client["matches"]
+
+
+if __name__ == "__main__":
+
+    # Get the database
+    dbname = get_mongoDB()
