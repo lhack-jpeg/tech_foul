4b7a41506a82bce210b45027992523274c0acf6e
lhack-jpeg <luke-hackett@outlook.com>
2023-03-02 12:55:48 +1100

    changed formatting of string

--- mongo_link/sql_mongo_link.py
+++ mongo_link/sql_mongo_link.py
@@ -129,7 +129,7 @@ def get_stats(matches_dict):
     team_dict["avg_match_time"] = sum(match_duration) / 10
     match_seconds = team_dict['avg_match_time'] % (24 * 3600)
     match_minutes = match_seconds // 60
-    team_dict["avg_match_time"] = f"{match_minutes}:{match_seconds}"
+    team_dict["avg_match_time"] = f"{match_minutes}:{match_seconds:2f}"
     team_dict["avg_score"] = sum(average_score) / 10
     team_dict["first_blood_pct"] = sum(first_bloods) / 10
     team_dict["prev_form"] = prev_performance
