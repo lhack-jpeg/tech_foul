3eb235fc0c13a4163674bed5c81926c06ea4476e
lhack-jpeg <luke-hackett@outlook.com>
2023-03-03 11:04:22 +1100

    change match_id hash function to take tournament name

--- dota_match_scheduler/dota_match_scheduler/create_match_id.py
+++ dota_match_scheduler/dota_match_scheduler/create_match_id.py
@@ -5,12 +5,12 @@ for the match id.
 from hashlib import md5
 
 
-def create_match_id(team_one, team_two, start_time):
+def create_match_id(team_one, team_two, tournament):
     '''
     takes the three values and hashes them using the md5 function to return a
     a hexideciaml string.
     '''
-    match_id = f"{str(team_one)}{str(team_two)}{str(start_time)}".encode()
+    match_id = f"{str(team_one)}{str(team_two)}{str(tournament)}".encode()
     match_id = md5(match_id)
     match_id = str(match_id.hexdigest())
     return match_id
